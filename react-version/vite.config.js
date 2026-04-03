163519e513b84f2af794aaa2673d19d6576d0cec
Praveen K <praveenparker1510@gmail.com>
2026-04-03 18:41:18 +0530

    Update vite.config.js

--- react-version/vite.config.js
+++ react-version/vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // ensures proper routing on Vercel/Netlify
+  build: {
+    outDir: 'dist', // publish folder
+  }
+});
